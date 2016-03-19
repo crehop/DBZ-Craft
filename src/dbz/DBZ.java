59d1dfbb979d82dd16be9443f6a4420869ccc1ff
Dolphindalt <dolphindalt@gmail.com>
2016-03-19 16:03:56 -0700

    Reload Additions
    Clean reloads.

--- src/dbz/DBZ.java
+++ src/dbz/DBZ.java
@@ -1,12 +1,15 @@
 package dbz;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import dbz.listeners.DoubleTapListener;
 import dbz.listeners.InteractEventListener;
 import dbz.listeners.PlayerJoinListener;
+import dbz.player.DBZPlayer;
+import dbz.player.PlayerHandler;
 
 public class DBZ extends JavaPlugin {
 
@@ -18,11 +21,13 @@ public class DBZ extends JavaPlugin {
 		registerEvents();
 		registerTasks();
 		
+		addAllOnlinePlayers();
+		
 		Bukkit.getServer().broadcastMessage("==========DBZ PLUGIN LOADED==========");
 	}
 	
 	public void onDisable() {
-		
+		PlayerHandler.dBZPlayers.clear();
 	}
 	
 	public void registerCommands() {
@@ -39,5 +44,11 @@ public class DBZ extends JavaPlugin {
 	public void registerTasks() {
 		this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new HeartBeat(), 0L, 1L);
 	}
+	
+	public void addAllOnlinePlayers() {
+		for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+			PlayerHandler.addDBZPlayer(new DBZPlayer(p));
+		}
+ 	}
 	
 }
