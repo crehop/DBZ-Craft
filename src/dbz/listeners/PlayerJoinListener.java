0ebb453453ecd6e97afe9bfa96a3557e69157734
Crehop <crehop2@gmail.com>
2016-03-19 16:02:03 -0700

    Synced

--- src/dbz/listeners/PlayerJoinListener.java
+++ src/dbz/listeners/PlayerJoinListener.java
@@ -1,5 +1,6 @@
 package dbz.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -11,5 +12,6 @@ public class PlayerJoinListener implements Listener {
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent e) {
 		PlayerHandler.addDBZPlayer(e.getPlayer());
+		Bukkit.broadcastMessage("ADDED");
 	}
 }
